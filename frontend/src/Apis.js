29c0d1468dffe233157c1693d9574258a7a38c4c
vishalkushwah09 <vishalji.kus.login@gmail.com>
2025-06-27 10:39:39 +0530

    Fix API'S

--- frontend/src/Apis.js
+++ frontend/src/Apis.js
@@ -1,72 +1,73 @@
-export default {
-    ADMIN_SIGNUP : "http://localhost:4000/admin/SignUp",
-    ADMIN_SIGNIN : "http://localhost:4000/admin/SignIn",
-    ADMIN_PROFILE : "http://localhost:4000/admin/profile",
-    ADMIN_VERIFY :"http://localhost:4000/admin/verify-otp",
-     
-    ADMIN_CREATE_BATCH : "http://localhost:4000/admin/batch",
-    ADMIN_BATCHES : "http://localhost:4000/admin/batch",
-    ADMIN_Delete_BATCH : "http://localhost:4000/admin/removeBatch",
-    
-    ADMIN_ADDONE:"http://localhost:4000/admin/addOneUser",
-    ADMIN_USER_BULK : "http://localhost:4000/admin/userBulk",
-    ADMIN_USER_DELETE : "http://localhost:4000/admin/removeOne",
-
-    ADMIN_LEAVES : "http://localhost:4000/admin/leave",
-    ADMIN_LEAVESTATUS :"http://localhost:4000/admin/status",
-
-    ADMIN_USERS : "http://localhost:4000/admin/allUser",
-    ADMIN_USER_BATCH :"http://localhost:4000/admin/viewAttendance",
-
-
-    ADMIN_QR :"http://localhost:4000/admin/qr",
-    
-    ADMIN_HOLIDAY : "http://localhost:4000/admin/holiday",
-   ADMIN_FORGOT : "http://localhost:4000/admin/forgot",
-   ADMIN_RESET : "http://localhost:4000/admin/reset",
-
-    USER_SIGNIN : "http://localhost:4000/user/Login",
-    USER_PROFILE : "http://localhost:4000/user/details",
-    USER_VIEW : "http://localhost:4000/user/view",
-    USER_MARK :"http://localhost:4000/user/mark",
-    USER_HOLI :"http://localhost:4000/user/holiday",
-    USER_LEAVE :"http://localhost:4000/user/leave",
-    USER_COUNT : "http://localhost:4000/user/count"
-   
-}
 // export default {
-//     ADMIN_SIGNUP : "http://192.168.1.42:4000/admin/SignUp",
-//     ADMIN_SIGNIN : "http://192.168.1.42:4000/admin/SignIn",
-//     ADMIN_PROFILE : "http://192.168.1.42:4000/admin/profile",
-//     ADMIN_VERIFY :"http://192.168.1.42:4000/admin/verify-otp",
+//     ADMIN_SIGNUP : "http://localhost:4000/admin/SignUp",
+//     ADMIN_SIGNIN : "http://localhost:4000/admin/SignIn",
+//     ADMIN_PROFILE : "http://localhost:4000/admin/profile",
+//     ADMIN_VERIFY :"http://localhost:4000/admin/verify-otp",
      
-//     ADMIN_CREATE_BATCH : "http://192.168.1.42:4000/admin/batch",
-//     ADMIN_BATCHES : "http://192.168.1.42:4000/admin/batch",
-//     ADMIN_Delete_BATCH : "http://192.168.1.42:4000/admin/removeBatch",
+//     ADMIN_CREATE_BATCH : "http://localhost:4000/admin/batch",
+//     ADMIN_BATCHES : "http://localhost:4000/admin/batch",
+//     ADMIN_Delete_BATCH : "http://localhost:4000/admin/removeBatch",
     
-//     ADMIN_ADDONE:"http://192.168.1.42:4000/admin/addOneUser",
-//     ADMIN_USER_BULK : "http://192.168.1.42:4000/admin/userBulk",
-//     ADMIN_USER_DELETE : "http://192.168.1.42:4000/admin/removeOne",
+//     ADMIN_ADDONE:"http://localhost:4000/admin/addOneUser",
+//     ADMIN_USER_BULK : "http://localhost:4000/admin/userBulk",
+//     ADMIN_USER_DELETE : "http://localhost:4000/admin/removeOne",
 
-//     ADMIN_LEAVES : "http://192.168.1.42:4000/admin/leave",
-//     ADMIN_LEAVESTATUS :"http://192.168.1.42:4000/admin/status",
+//     ADMIN_LEAVES : "http://localhost:4000/admin/leave",
+//     ADMIN_LEAVESTATUS :"http://localhost:4000/admin/status",
 
-//     ADMIN_USERS : "http://192.168.1.42:4000/admin/allUser",
-//     ADMIN_USER_BATCH :"http://192.168.1.42:4000/admin/viewAttendance",
+//     ADMIN_USERS : "http://localhost:4000/admin/allUser",
+//     ADMIN_USER_BATCH :"http://localhost:4000/admin/viewAttendance",
 
 
-//     ADMIN_QR :"http://192.168.1.42:4000/admin/qr",
+//     ADMIN_QR :"http://localhost:4000/admin/qr",
     
-//     ADMIN_HOLIDAY : "http://192.168.1.42:4000/admin/holiday",
-//    ADMIN_FORGOT : "http://192.168.1.42:4000/admin/forgot",
-//    ADMIN_RESET : "http://192.168.1.42:4000/admin/reset",
+//     ADMIN_HOLIDAY : "http://localhost:4000/admin/holiday",
+//    ADMIN_FORGOT : "http://localhost:4000/admin/forgot",
+//    ADMIN_RESET : "http://localhost:4000/admin/reset",
 
-//     USER_SIGNIN : "http://192.168.1.42:4000/user/Login",
-//     USER_PROFILE : "http://192.168.1.42:4000/user/details",
-//     USER_VIEW : "http://192.168.1.42:4000/user/view",
-//     USER_MARK :"http://192.168.1.42:4000/user/mark",
-//     USER_HOLI :"http://192.168.1.42:4000/user/holiday",
-//     USER_LEAVE :"http://192.168.1.42:4000/user/leave",
-//     USER_COUNT : "http://192.168.1.42:4000/user/count"
+//     USER_SIGNIN : "http://localhost:4000/user/Login",
+//     USER_PROFILE : "http://localhost:4000/user/details",
+//     USER_VIEW : "http://localhost:4000/user/view",
+//     USER_MARK :"http://localhost:4000/user/mark",
+//     USER_HOLI :"http://localhost:4000/user/holiday",
+//     USER_LEAVE :"http://localhost:4000/user/leave",
+//     USER_COUNT : "http://localhost:4000/user/count"
    
 // }
+
+export default {
+    ADMIN_SIGNUP : "https://trackit-backend-ubqy.onrender.com/admin/SignUp",
+    ADMIN_SIGNIN : "https://trackit-backend-ubqy.onrender.com/admin/SignIn",
+    ADMIN_PROFILE : "https://trackit-backend-ubqy.onrender.com/admin/profile",
+    ADMIN_VERIFY :"https://trackit-backend-ubqy.onrender.com/admin/verify-otp",
+     
+    ADMIN_CREATE_BATCH : "https://trackit-backend-ubqy.onrender.com/admin/batch",
+    ADMIN_BATCHES : "https://trackit-backend-ubqy.onrender.com/admin/batch",
+    ADMIN_Delete_BATCH : "https://trackit-backend-ubqy.onrender.com/admin/removeBatch",
+    
+    ADMIN_ADDONE:"https://trackit-backend-ubqy.onrender.com/admin/addOneUser",
+    ADMIN_USER_BULK : "https://trackit-backend-ubqy.onrender.com/admin/userBulk",
+    ADMIN_USER_DELETE : "https://trackit-backend-ubqy.onrender.com/admin/removeOne",
+
+    ADMIN_LEAVES : "https://trackit-backend-ubqy.onrender.com/admin/leave",
+    ADMIN_LEAVESTATUS :"https://trackit-backend-ubqy.onrender.com/admin/status",
+
+    ADMIN_USERS : "https://trackit-backend-ubqy.onrender.com/admin/allUser",
+    ADMIN_USER_BATCH :"https://trackit-backend-ubqy.onrender.com/admin/viewAttendance",
+
+
+    ADMIN_QR :"https://trackit-backend-ubqy.onrender.com/admin/qr",
+    
+    ADMIN_HOLIDAY : "https://trackit-backend-ubqy.onrender.com/admin/holiday",
+   ADMIN_FORGOT : "https://trackit-backend-ubqy.onrender.com/admin/forgot",
+   ADMIN_RESET : "https://trackit-backend-ubqy.onrender.com/admin/reset",
+
+    USER_SIGNIN : "https://trackit-backend-ubqy.onrender.com/user/Login",
+    USER_PROFILE : "https://trackit-backend-ubqy.onrender.com/user/details",
+    USER_VIEW : "https://trackit-backend-ubqy.onrender.com/user/view",
+    USER_MARK :"https://trackit-backend-ubqy.onrender.com/user/mark",
+    USER_HOLI :"https://trackit-backend-ubqy.onrender.com/user/holiday",
+    USER_LEAVE :"https://trackit-backend-ubqy.onrender.com/user/leave",
+    USER_COUNT : "https://trackit-backend-ubqy.onrender.com/user/count"
+   
+}
