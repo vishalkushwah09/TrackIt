b5e6a65e37aa6c5759065fe067196fa85e6f1d42
vishalkushwah09 <vishalji.kus.login@gmail.com>
2025-06-27 11:26:57 +0530

    Fix server home route

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import "./db/dbConfi.js";
-import AdminRoute from "./routes/admin.routes.js";
-import userRouter from "./routes/user.route.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+    import express from "express";
+    import bodyParser from "body-parser";
+    import dotenv from "dotenv";
+    import "./db/dbConfi.js";
+    import AdminRoute from "./routes/admin.routes.js";
+    import userRouter from "./routes/user.route.js";
+    import cors from "cors";
+    import cookieParser from "cookie-parser";
 
 
 
-dotenv.config();
+    dotenv.config();
 
-const server = express();
-server.use(cors());
-server.use(cookieParser(    ))
+    const server = express();
+    server.use(cors());
+    server.use(cookieParser(    ))
 
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended:true}));
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({extended:true}));
 
-server.use("/",()=>{
-    console.log("backend live");
-})
-server.use("/admin",AdminRoute);
-server.use("/user",userRouter);
+    server.use("/",(req,res)=>{
+        console.log("backend live");
+    })
+    server.use("/admin",AdminRoute);
+    server.use("/user",userRouter);
 
-server.listen(process.env.PORT,"0.0.0.0",()=>{
-    console.log("server started");
-})
+    server.listen(process.env.PORT,"0.0.0.0",()=>{
+        console.log("server started");
+    })
 
