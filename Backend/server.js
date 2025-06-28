805137aec905cb76d424e6867a68b0cf2a5291c5
vishalkushwah09 <vishalji.kus.login@gmail.com>
2025-06-28 10:14:46 +0530

    Update cors

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,24 @@
     dotenv.config();
 
     const server = express();
-    server.use(cors());
+const allowedOrigins = [
+  "https://trackit-frontend-tauz.onrender.com",  
+  "http://localhost:3000"                        
+];
+
+server.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
+
+server.options("*", cors());
     server.use(cookieParser(    ))
 
     server.use(bodyParser.json());
