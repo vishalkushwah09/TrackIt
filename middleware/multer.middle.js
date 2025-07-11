9d510aeb95c684abd78017bdc136d742739ea0f2
vishalji <vishalji.kus.login@gmail.com>
2025-07-11 11:12:00 +0530

    Add multer middleware file

--- middleware/multer.middle.js
+++ middleware/multer.middle.js
@@ -5,6 +5,7 @@ import path from "path";
     destination :(req,file,cb)=>cb(null,'/tmp'),
     filename:(req,file,cb)=>cb(null,Date.now()+ path.extname(file.originalname))
  });
- const upload = multer({storage});
  
+ const upload = multer({storage});
+
  export default upload;
